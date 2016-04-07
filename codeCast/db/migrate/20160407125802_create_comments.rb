a7c5f0e2ac114014ce3a2818a655639b094681bb
Stan <stan.amsellem@gmail.com>
2016-04-07 14:58:40 +0200

    create comments table

---
+++ codeCast/db/migrate/20160407125802_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+
+      t.text :body, null: false
+      t.integer :user_id, null: false, index: true
+      t.integer :cast_id, null: false, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- codeCast/db/schema.rb
+++ codeCast/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407125702) do
+ActiveRecord::Schema.define(version: 20160407125802) do
 
   create_table "casts", force: :cascade do |t|
     t.string   "title",      null: false
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20160407125702) do
 
   add_index "casts", ["tagging_id"], name: "index_casts_on_tagging_id"
   add_index "casts", ["user_id"], name: "index_casts_on_user_id"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.integer  "user_id",    null: false
+    t.integer  "cast_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["cast_id"], name: "index_comments_on_cast_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "ratings", force: :cascade do |t|
     t.integer  "rating",     null: false
